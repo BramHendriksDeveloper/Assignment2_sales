00ffa9a70d07b84311ff28ad692ce18796a13bd4
BramHendriksDeveloper <bram5688@gmail.com>
2016-09-13 13:52:00 +0200

    project en retailers

--- js/chart-data.js
+++ js/chart-data.js
@@ -71,6 +71,32 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 				}
 			]
 		}
+
+	var lineChartData3 = {
+			labels : ["January","February","March","April","May","June","July"],
+			datasets : [
+				{
+					label: "My First dataset",
+					fillColor : "rgba(48, 86, 255, 0.2)",
+					strokeColor : "rgba(48, 86, 255, 1)",
+					pointColor : "rgba(48, 86, 255, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					data : [80,290,320,450,520,600,630]
+				},
+				{
+					label: "My First dataset",
+					fillColor : "rgba(24, 158, 82, 0.2)",
+					strokeColor : "rgba(24, 158, 82, 1)",
+					pointColor : "rgba(24, 158, 82, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					data : [100,200,300,400,500,600,700]
+				}
+			]
+		}
 		
 	var barChartData = {
 			labels : ["January","February","March","April","May","June","July"],
@@ -80,14 +106,14 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 					strokeColor : "rgba(48, 164, 255, 0.8)",
 					highlightFill : "rgba(48, 164, 255, 0.75)",
 					highlightStroke : "rgba(48, 164, 255, 1)",
-					data : [200,180,212,120,276,205,188]
+					data : [280,250,230,230,230,230,230]
 				},
 				{
-					fillColor : "rgba(255,170,0, 0.2)",
-					strokeColor : "rgba(255,170,0, 0.8)",
-					highlightFill : "rgba(255,170,0, 0.75)",
-					highlightStroke : "rgba(255,170,0, 1)",
-					data : [30,10,13,44,67,2,10]
+					fillColor : "rgba(24, 158, 82, 0.2)",
+					strokeColor : "rgba(24, 158, 82, 0.8)",
+					highlightFill : "rgba(24, 158, 82, 0.75)",
+					highlightStroke : "rgba(24, 158, 82, 1)",
+					data : [293,244,245,157,144,240,210]
 				},
 				{
 					fillColor : "rgba(255,51,0,0.5)",
@@ -175,6 +201,13 @@ window.onload = function(){
 	if(chart2){
 		chart2 = chart2.getContext("2d");
 		window.myBar = new Chart(chart2).Bar(barChartData, {
+			responsive : true
+		});
+	}	
+	var chart3 = document.getElementById("line3-chart");
+	if(chart3){
+		chart3 = chart3.getContext("2d");
+		window.myBar = new Chart(chart3).Line(lineChartData3, {
 			responsive : true
 		});
 	}	
