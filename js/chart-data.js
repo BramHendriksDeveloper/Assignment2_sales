61252aca04e9ed1806d2725b64b1db655f128cdb
BramHendriksDeveloper <bram5688@gmail.com>
2016-09-19 21:03:07 +0200

    19-sep

--- js/chart-data.js
+++ js/chart-data.js
@@ -97,6 +97,152 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 				}
 			]
 		}
+
+	var lineChartData4 = {
+			labels : ["January","February","March","April","May","June","July"],
+			datasets : [
+				{
+					label: "My First dataset",
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 1)",
+					pointColor : "rgba(48, 164, 255, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					data : [20,23,18,26,30,36,35]
+				},
+				{
+					label: "My First dataset",
+					fillColor : "rgba(24, 158, 82, 0.2)",
+					strokeColor : "rgba(24, 158, 82, 1)",
+					pointColor : "rgba(24, 158, 82, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					data : [12,11,13,18,23,29,29]
+				}
+			]
+		}
+
+	var lineChartData5 = {
+			labels : ["January","February","March","April","May","June","July","Augustus","September","Oktober","November","December"],
+			datasets : [
+				{
+					label: "My First dataset",
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 1)",
+					pointColor : "rgba(48, 164, 255, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					data : [1,3,2,4,2,0,0,6,4,2,3,2]
+				}
+			]
+		}
+
+	var lineChartData6 = {
+			labels : ["January","February","March","April","May","June","July","Augustus","September","Oktober","November","December"],
+			datasets : [
+				{
+					label: "My First dataset",
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 1)",
+					pointColor : "rgba(48, 164, 255, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					data : [15,14,16,13,15,12,11,18,14,15,20,19]
+				},
+				{
+					label: "My First dataset",
+					fillColor : "rgba(24, 158, 82, 0.2)",
+					strokeColor : "rgba(24, 158, 82, 1)",
+					pointColor : "rgba(24, 158, 82, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(24, 158, 82, 1)",
+					data : [2,1,3,1,2,2,0,0,3,2,5,4]
+				}
+			]
+		}
+
+	var lineChartData7 = {
+			labels : ["January","February","March","April","May","June","July"],
+			datasets : [
+				{
+					label: "My First dataset",
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 1)",
+					pointColor : "rgba(48, 164, 255, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					data : [100,45,89,85,124,56,62]
+				}
+			]
+		}
+
+	var lineChartData8 = {
+			labels : ["January","February","March","April","May","June","July"],
+			datasets : [
+				{
+					label: "My First dataset",
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 1)",
+					pointColor : "rgba(48, 164, 255, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					data : [200,94,153,158,248,121,124]
+				}
+			]
+		}
+
+	var lineChartData9 = {
+			labels : ["January","February","March","April","May","June","July"],
+			datasets : [
+				{
+					label: "My First dataset",
+					fillColor : "rgba(24, 158, 82, 0.2)",
+					strokeColor : "rgba(24, 158, 82, 1)",
+					pointColor : "rgba(24, 158, 82, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(24, 158, 82, 1)",
+					data : [15,17,20,24,28,29,29]
+				},
+				{
+					label: "My First dataset",
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 1)",
+					pointColor : "rgba(48, 164, 255, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(48, 164, 255, 1)",
+					data : [45,44,43,41,39,38,38]
+				},
+				{
+					label: "My First dataset",
+					fillColor : "rgba(255,170,0, 0.2)",
+					strokeColor : "rgba(255,170,0, 1)",
+					pointColor : "rgba(255,170,0, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(255,170,0, 1)",
+					data : [35,34,33,31,30,30,30]
+				},
+				{
+					label: "My First dataset",
+					fillColor : "rgba(255,51,0, 0.2)",
+					strokeColor : "rgba(255,51,0, 1)",
+					pointColor : "rgba(255,51,0, 1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(255,51,0, 1)",
+					data : [5,5,4,4,3,3,3]
+				}
+			]
+		}
 		
 	var barChartData = {
 			labels : ["January","February","March","April","May","June","July"],
@@ -182,6 +328,132 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 	
 		}
 
+	var barChartData5 = {
+			labels : ["Augustus","September","Oktober","November","December"],
+			datasets : [
+				{
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 0.8)",
+					highlightFill : "rgba(48, 164, 255, 0.75)",
+					highlightStroke : "rgba(48, 164, 255, 1)",
+					data : [2000,2000,6000,16000,20000]
+				},
+				{
+					fillColor : "rgba(24, 158, 82, 0.2)",
+					strokeColor : "rgba(24, 158, 82, 0.8)",
+					highlightFill : "rgba(24, 158, 82, 0.75)",
+					highlightStroke : "rgba(24, 158, 82, 1)",
+					data : [10000,10000,15000,15000,15000]
+				}
+			]
+	
+		}
+
+	var barChartData6 = {
+			labels : ["Augustus","September","Oktober","November","December"],
+			datasets : [
+				{
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 0.8)",
+					highlightFill : "rgba(48, 164, 255, 0.75)",
+					highlightStroke : "rgba(48, 164, 255, 1)",
+					data : [5000,5000,5000,5000,5000]
+				},
+				{
+					fillColor : "rgba(24, 158, 82, 0.2)",
+					strokeColor : "rgba(24, 158, 82, 0.8)",
+					highlightFill : "rgba(24, 158, 82, 0.75)",
+					highlightStroke : "rgba(24, 158, 82, 1)",
+					data : [5000,5000,4600,3500,1000]
+				},
+				{
+					fillColor : "rgba(255,51,0, 0.2)",
+					strokeColor : "rgba(255,51,0, 0.8)",
+					highlightFill : "rgba(255,51,0, 0.75)",
+					highlightStroke : "rgba(255,51,0, 1)",
+					data : [0,0,400,1500,4000]
+				}
+			]
+	
+		}
+
+	var barChartData7 = {
+			labels : ["Augustus","September","Oktober","November","December"],
+			datasets : [
+				{
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 0.8)",
+					highlightFill : "rgba(48, 164, 255, 0.75)",
+					highlightStroke : "rgba(48, 164, 255, 1)",
+					data : [1,0,1,1,0]
+				},
+				{
+					fillColor : "rgba(24, 158, 82, 0.2)",
+					strokeColor : "rgba(24, 158, 82, 0.8)",
+					highlightFill : "rgba(24, 158, 82, 0.75)",
+					highlightStroke : "rgba(24, 158, 82, 1)",
+					data : [2,2,4,5,1]
+				},
+				{
+					fillColor : "rgba(255,170,0, 0.2)",
+					strokeColor : "rgba(255,170,0, 0.8)",
+					highlightFill : "rgba(255,170,0, 0.75)",
+					highlightStroke : "rgba(255,170,0, 1)",
+					data : [1,1,1,1,2]
+				}
+			]
+	
+		}
+
+	var barChartData8 = {
+			labels : ["January","February","March","April","May","June","July"],
+			datasets : [
+				{
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 0.8)",
+					highlightFill : "rgba(48, 164, 255, 0.75)",
+					highlightStroke : "rgba(48, 164, 255, 1)",
+					data : [400,300,300,300,300,340,340]
+				},
+				{
+					fillColor : "rgba(24, 158, 82, 0.2)",
+					strokeColor : "rgba(24, 158, 82, 0.8)",
+					highlightFill : "rgba(24, 158, 82, 0.75)",
+					highlightStroke : "rgba(24, 158, 82, 1)",
+					data : [0,40,5,57,0,18,186]
+				},
+				{
+					fillColor : "rgba(255,51,0, 0.2)",
+					strokeColor : "rgba(255,51,0, 0.8)",
+					highlightFill : "rgba(255,51,0, 0.75)",
+					highlightStroke : "rgba(255,51,0, 1)",
+					data : [450,260,295,243,420,322,154]
+				}
+			]
+	
+		}
+
+	var barChartData9 = {
+			labels : ["Augustus","September","Oktober","November","December"],
+			datasets : [
+				{
+					fillColor : "rgba(48, 164, 255, 0.2)",
+					strokeColor : "rgba(48, 164, 255, 0.8)",
+					highlightFill : "rgba(48, 164, 255, 0.75)",
+					highlightStroke : "rgba(48, 164, 255, 1)",
+					data : [2000,2000,6000,16000,20000]
+				},
+				{
+					fillColor : "rgba(24, 158, 82, 0.2)",
+					strokeColor : "rgba(24, 158, 82, 0.8)",
+					highlightFill : "rgba(24, 158, 82, 0.75)",
+					highlightStroke : "rgba(24, 158, 82, 1)",
+					data : [10000,10000,15000,15000,15000]
+				}
+			]
+	
+		}
+
 	var pieData = [
 				{
 					value: 300,
@@ -259,7 +531,7 @@ window.onload = function(){
 		window.myBar = new Chart(chart2).Bar(barChartData, {
 			responsive : true
 		});
-	}	
+	}
 	var chart3 = document.getElementById("line3-chart");
 	if(chart3){
 		chart3 = chart3.getContext("2d");
@@ -287,5 +559,82 @@ window.onload = function(){
 		window.myBar = new Chart(chart7).Bar(barChartData4, {
 			responsive : true
 		});
-	}	
+	}
+	var chart8 = document.getElementById("bar5-chart");
+	if(chart8){
+		chart8 = chart8.getContext("2d");
+		window.myBar = new Chart(chart8).Bar(barChartData5, {
+			responsive : true
+		});
+	}
+	var chart9 = document.getElementById("line4-chart");
+	if(chart9){
+		chart9 = chart9.getContext("2d");
+		window.myBar = new Chart(chart9).Line(lineChartData4, {
+			responsive : true
+		});
+	}
+	var chart10 = document.getElementById("bar6-chart");
+	if(chart10){
+		chart10 = chart10.getContext("2d");
+		window.myBar = new Chart(chart10).Bar(barChartData6, {
+			responsive : true
+		});
+	}
+	var chart11 = document.getElementById("line5-chart");
+	if(chart11){
+		chart11 = chart11.getContext("2d");
+		window.myBar = new Chart(chart11).Line(lineChartData5, {
+			responsive : true
+		});
+	}
+	var chart12 = document.getElementById("bar7-chart");
+	if(chart12){
+		chart12 = chart12.getContext("2d");
+		window.myBar = new Chart(chart12).Bar(barChartData7, {
+			responsive : true
+		});
+	}
+	var chart13 = document.getElementById("line6-chart");
+	if(chart13){
+		chart13 = chart13.getContext("2d");
+		window.myBar = new Chart(chart13).Line(lineChartData6, {
+			responsive : true
+		});
+	}
+	var chart13 = document.getElementById("bar8-chart");
+	if(chart13){
+		chart13 = chart13.getContext("2d");
+		window.myBar = new Chart(chart13).Bar(barChartData8, {
+			responsive : true
+		});
+	}
+	var chart14 = document.getElementById("line7-chart");
+	if(chart14){
+		chart14 = chart14.getContext("2d");
+		window.myBar = new Chart(chart14).Line(lineChartData7, {
+			responsive : true
+		});
+	}
+	var chart15 = document.getElementById("line8-chart");
+	if(chart15){
+		chart15 = chart15.getContext("2d");
+		window.myBar = new Chart(chart15).Line(lineChartData8, {
+			responsive : true
+		});
+	}
+	var chart16 = document.getElementById("line9-chart");
+	if(chart16){
+		chart16 = chart16.getContext("2d");
+		window.myBar = new Chart(chart16).Line(lineChartData9, {
+			responsive : true
+		});
+	}
+	var chart17 = document.getElementById("bar9-chart");
+	if(chart17){
+		chart17 = chart17.getContext("2d");
+		window.myBar = new Chart(chart17).Bar(barChartData9, {
+			responsive : true
+		});
+	}
 };
